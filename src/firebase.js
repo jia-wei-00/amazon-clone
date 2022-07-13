fe017d3e2a44a665f28a4279ddc9bbdcd77cff4c
asdfcstgh <wwwbingo6@gmail.com>
2022-07-14 01:02:55 +0800

    add all

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNOvI2OGYwXb5ATLuvJYBSbOvys3ecN-A",
+  authDomain: "fir-e90c6.firebaseapp.com",
+  projectId: "fir-e90c6",
+  storageBucket: "fir-e90c6.appspot.com",
+  messagingSenderId: "1056567556404",
+  appId: "1:1056567556404:web:d77782f9657d74bf680d61",
+  measurementId: "G-EX7KSQKHGP",
+};
